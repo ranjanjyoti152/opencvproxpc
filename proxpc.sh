1a6eaf5e8a326ca4d1a808b2e247349f6e9a523b
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-01-20 14:32:16 +0530

    Update proxpc.sh

--- proxpc.sh
+++ proxpc.sh
@@ -23,6 +23,10 @@ sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt install nvidia-driver-520 -y
 sudo dpkg --configure -a
 gsettings set org.gnome.desktop.background picture-uri https://raw.githubusercontent.com/ranjanjyoti152/opencvproxpc/main/Wallpaper-01.jpg
+sudo wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+sudo echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+sudo apt update
+sudo apt install anydesk
 
 echo "**********************************************************************************************************************************************"
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>                                                                   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
