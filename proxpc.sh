3e6532a20156247256dec826b14b94f6f9e79dc2
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-02-01 19:28:06 +0530

    Update proxpc.sh

--- proxpc.sh
+++ proxpc.sh
@@ -8,6 +8,7 @@ echo "##########################################################################
 sudo apt update 
 sudo apt upgrade -y 
 sudo apt autoremove -y
+sudo apt install git -y
 sudo apt install net-tools -y
 sudo apt install openssh-server -y
 sudo apt install curl -y
