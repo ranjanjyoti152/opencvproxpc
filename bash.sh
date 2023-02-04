85909548cc81d00db2dba3b64ce598d59558f180
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-02-04 01:14:57 -0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -11,9 +11,9 @@ sudo apt-get install nvidia-driver-450 -y
 
 # Install AI-related applications
 sudo apt-get install python3-pip -y
-pip3 install numpy scipy pandas matplotlib -y
-pip3 install tensorflow keras pytorch -y
-pip3 install opencv-python-headless opencv-python-cuda -y
+pip3 install numpy scipy pandas matplotlib
+pip3 install tensorflow keras pytorch
+pip3 install opencv-python-headless opencv-python-cuda
 
 # Update the system once again
 sudo apt-get update -y
@@ -21,16 +21,16 @@ sudo apt-get upgrade -y
 
 # Install CUDA dependencies
 sudo apt-get update
-sudo apt-get install build-essential
-sudo apt-get install freeglut3-dev
-sudo apt-get install libx11-dev
-sudo apt-get install libxi-dev
-sudo apt-get install libxmu-dev
-sudo apt-get install libgl1-mesa-glx
-sudo apt-get install libglu1-mesa
-sudo apt-get install libglu1-mesa-dev
-sudo apt-get install libglfw3
-sudo apt-get install libglfw3-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install freeglut3-dev
+sudo apt-get -y install libx11-dev
+sudo apt-get -y install libxi-dev
+sudo apt-get -y install libxmu-dev
+sudo apt-get -y install libgl1-mesa-glx
+sudo apt-get -y install libglu1-mesa
+sudo apt-get -y install libglu1-mesa-dev
+sudo apt-get -y install libglfw3
+sudo apt-get -y install libglfw3-dev
 
 # Download CUDA Toolkit
 wget https://developer.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
