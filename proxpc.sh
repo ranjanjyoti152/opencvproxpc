f9b7e7839448736eec4b9fa8a6d1c17dd5ffb4c3
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-03-24 12:14:10 +0530

    Update proxpc.sh

--- proxpc.sh
+++ proxpc.sh
@@ -13,10 +13,7 @@ sudo apt install git -y
 sudo apt install net-tools -y
 sudo apt install openssh-server -y
 sudo apt install curl -y
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update -y
-sudo apt install brave-browser -y
+
 
 echo "Installation Of Brave Browser Completed"
 
