b1fcdb0d86fa654651eb76ced00fcfea2e1b1925
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-05-06 01:33:28 +0530

    Update CUDA.sh

--- CUDA.sh
+++ CUDA.sh
@@ -55,4 +55,4 @@ tar -xvf cudnn-linux-x86_64-8.9.1.23_cuda12-archive.tar.xz
 sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include 
 sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64 
 sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
-
+sudo apt -y install nvidia-cuda-toolkit
