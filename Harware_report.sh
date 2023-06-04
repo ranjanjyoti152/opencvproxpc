e919e6f944b6ad561c2067f234c0dda6d6580df3
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-06-04 14:54:56 +0530

    Update Harware_report.sh

--- Harware_report.sh
+++ Harware_report.sh
@@ -5,7 +5,18 @@ echo "Hardware Report"
 echo "---------------"
 echo "Processor: $(grep "model name" /proc/cpuinfo | uniq | sed -e 's/model name.*: //' -e 's/ @.*//')"
 echo "RAM: $(free -h | awk '/Mem:/ {print $2}')"
-echo "Disk Space: $(df -h / | awk '/\// {print $2 " total, " $3 " used, " $4 " available"}')"
+echo "RAM Diagnostics:"
+echo "$(sudo dmidecode --type 17 | grep Size | awk '{sum += $2} END {print "Total Installed RAM: " sum " MB"}')"
+echo "$(sudo dmidecode --type 17 | grep Speed | awk '{print "Speed: " $2 " MHz"}')"
+echo
+
+echo "Motherboard: $(sudo dmidecode -s baseboard-product-name)"
+echo "Motherboard Diagnostics:"
+echo "$(sudo dmidecode -s baseboard-manufacturer)"
+echo "$(sudo dmidecode -s baseboard-version)"
+echo
+
+echo "LAN Ports: $(lshw -class network | awk '/logical name/ {print $3}')"
 echo
 
 # Software Report
