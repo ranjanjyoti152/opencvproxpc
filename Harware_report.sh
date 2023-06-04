8aedc55857e4620aaeae89e96bb97539dd0d70ee
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-06-04 15:00:10 +0530

    Update Harware_report.sh

--- Harware_report.sh
+++ Harware_report.sh
@@ -29,3 +29,95 @@ echo
 
 # End of Report
 echo "Report generation complete."
+
+#!/bin/bash
+
+# Function to check hardware status
+check_hardware_status() {
+    echo "Hardware Status"
+    echo "---------------"
+
+    # CPU
+    echo "CPU Status:"
+    if grep -qi "model name" /proc/cpuinfo; then
+        echo "  CPU: OK"
+    else
+        echo "  CPU: Not Found"
+    fi
+
+    # RAM
+    echo "RAM Status:"
+    if free -h | awk '/Mem:/ {print $2}' >/dev/null; then
+        echo "  RAM: OK"
+    else
+        echo "  RAM: Not Found"
+    fi
+
+    # Storage Devices
+    echo "Storage Devices Status:"
+    if lsblk >/dev/null; then
+        echo "  Storage Devices: OK"
+    else
+        echo "  Storage Devices: Not Found"
+    fi
+
+    # GPU
+    echo "GPU Status:"
+    if lspci | grep -i vga >/dev/null && sudo lshw -C display >/dev/null; then
+        echo "  GPU: OK"
+    else
+        echo "  GPU: Not Found"
+    fi
+
+    # Network Interfaces
+    echo "Network Interfaces Status:"
+    if ip -o link show >/dev/null; then
+        echo "  Network Interfaces: OK"
+    else
+        echo "  Network Interfaces: Not Found"
+    fi
+
+    echo
+}
+
+# Function to check software status
+check_software_status() {
+    echo "Software Status"
+    echo "---------------"
+
+    # Operating System
+    echo "Operating System Status:"
+    if lsb_release -d | awk -F"\t" '{print $2}' >/dev/null; then
+        echo "  Operating System: OK"
+    else
+        echo "  Operating System: Not Found"
+    fi
+
+    # Kernel Version
+    echo "Kernel Version Status:"
+    if uname -r >/dev/null; then
+        echo "  Kernel Version: OK"
+    else
+        echo "  Kernel Version: Not Found"
+    fi
+
+    # Installed Packages
+    echo "Installed Packages Status:"
+    if dpkg --list | wc -l >/dev/null; then
+        echo "  Installed Packages: OK"
+    else
+        echo "  Installed Packages: Not Found"
+    fi
+
+    echo
+}
+
+# Check hardware status
+check_hardware_status
+
+# Check software status
+check_software_status
+
+# End of Report
+echo "System status check complete."
+
