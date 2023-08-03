56aa867c63ceff333272b0016787e85a5e6ec9dd
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-08-03 12:25:19 +0530

    Update proxpc-opencv.sh

--- proxpc-opencv.sh
+++ proxpc-opencv.sh
@@ -1,109 +1,66 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install nano
-sudo apt-get install dphys-swapfile -y
-sudo nano /sbin/dphys-swapfile
-free -m
-#!/bin/bash
-set -e
+sudo apt-get install build-essential cmake git unzip pkg-config -y
+sudo apt-get install libjpeg-dev libpng-dev libtiff-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev -y
+sudo apt-get install libgtk2.0-dev libcanberra-gtk* -y
+sudo apt-get install python3-dev python3-numpy python3-pip -y
+sudo apt-get install libxvidcore-dev libx264-dev libgtk-3-dev -y
+sudo apt-get install libtbb2 libtbb-dev libdc1394-22-dev -y
+sudo apt-get install libv4l-dev v4l-utils -y
 
-echo "Installing OpenCV 4.8.0 on your Jetson Nano"
-echo "It will take 3.5 hours !"
-
-# reveal the CUDA location
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev -y
+sudo apt-get install libavresample-dev libvorbis-dev libxine2-dev -y 
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev -y
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev -y
+sudo apt-get install libopenblas-dev libatlas-base-dev libblas-dev -y
+sudo apt-get install liblapack-dev libeigen3-dev gfortran -y
+sudo apt-get install libhdf5-dev protobuf-compiler -y
+sudo apt-get install libprotobuf-dev libgoogle-glog-dev libgflags-dev -y
+cd /usr/include/linux
+sudo ln -s -f ../libv4l1-videodev.h videodev.h
 cd ~
-sudo sh -c "echo '/usr/local/cuda/lib64' >> /etc/ld.so.conf.d/nvidia-tegra.conf"
-sudo ldconfig
-
-# install the dependencies
-sudo apt-get install -y build-essential cmake git unzip pkg-config zlib1g-dev
-sudo apt-get install -y libjpeg-dev libjpeg8-dev libjpeg-turbo8-dev libpng-dev libtiff-dev
-sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libglew-dev
-sudo apt-get install -y libgtk2.0-dev libgtk-3-dev libcanberra-gtk*
-sudo apt-get install -y python3-dev python3-numpy python3-pip
-sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
-sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev libxine2-dev
-sudo apt-get install -y gstreamer1.0-tools libv4l-dev v4l-utils qv4l2 
-sudo apt-get install -y libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev
-sudo apt-get install -y libavresample-dev libvorbis-dev libxine2-dev libtesseract-dev
-sudo apt-get install -y libfaac-dev libmp3lame-dev libtheora-dev libpostproc-dev
-sudo apt-get install -y libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
-sudo apt-get install -y liblapack-dev liblapacke-dev libeigen3-dev gfortran
-sudo apt-get install -y libhdf5-dev protobuf-compiler
-sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
-
-# remove old versions or previous builds
-cd ~ 
-sudo rm -rf opencv*
-# download the latest version
-wget -O opencv.zip https://github.com/opencv/opencv/archive/4.8.0.zip 
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.8.0.zip 
-# unpack
-unzip opencv.zip 
-unzip opencv_contrib.zip 
-# some administration to make live easier later on
-mv opencv-4.8.0 opencv
-mv opencv_contrib-4.8.0 opencv_contrib
-# clean up the zip files
-rm opencv.zip
-rm opencv_contrib.zip
-
-# set install dir
-cd ~/opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.4.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.4.0.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+mv opencv-4.4.0 opencv
+mv opencv_contrib-4.4.0 opencv_contrib
+cd opencv
 mkdir build
 cd build
-
-# run cmake
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D CMAKE_INSTALL_PREFIX=/usr \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
 -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
--D EIGEN_INCLUDE_PATH=/usr/include/eigen3 \
--D WITH_OPENCL=OFF \
--D WITH_CUDA=ON \
--D CUDA_ARCH_BIN=8.6 \
--D CUDA_ARCH_PTX="" \
--D WITH_CUDNN=ON \
--D WITH_CUBLAS=ON \
--D ENABLE_FAST_MATH=ON \
--D CUDA_FAST_MATH=ON \
--D OPENCV_DNN_CUDA=ON \
--D ENABLE_NEON=ON \
--D WITH_QT=OFF \
--D WITH_OPENMP=ON \
 -D BUILD_TIFF=ON \
 -D WITH_FFMPEG=ON \
 -D WITH_GSTREAMER=ON \
 -D WITH_TBB=ON \
 -D BUILD_TBB=ON \
--D BUILD_TESTS=OFF \
 -D WITH_EIGEN=ON \
 -D WITH_V4L=ON \
 -D WITH_LIBV4L=ON \
--D WITH_PROTOBUF=ON \
+-D WITH_VTK=OFF \
+-D WITH_QT=OFF \
+-D WITH_OPENGL=ON \
 -D OPENCV_ENABLE_NONFREE=ON \
 -D INSTALL_C_EXAMPLES=OFF \
 -D INSTALL_PYTHON_EXAMPLES=OFF \
--D PYTHON3_PACKAGES_PATH=/usr/lib/python3/dist-packages \
+-D BUILD_NEW_PYTHON_SUPPORT=ON \
 -D OPENCV_GENERATE_PKGCONFIG=ON \
+-D BUILD_TESTS=OFF \
+-D OPENCV_DNN_CUDA=ON \
+-D ENABLE_FAST_MATH=ON \
+-D CUDA_FAST_MATH=ON \
+-D CUDA_ARCH_BIN=8.6 \
+-D WITH_CUBLAS=ON \
+-D WITH_CUDNN=ON \
+-D CUDNN_LIBRARY=/usr/local/cuda/lib64/libcudnn.so.7.6.5 \
+-D CUDNN_INCLUDE_DIR=/usr/local/cuda/include \
 -D BUILD_EXAMPLES=OFF ..
 
-# run make
-FREE_MEM="$(free -m | awk '/^Swap/ {print $2}')"
-# Use "-j 4" only swap space is larger than 5.5GB
-if [[ "FREE_MEM" -gt "5500" ]]; then
-  NO_JOB=4
-else
-  echo "Due to limited swap, make only uses 1 core"
-  NO_JOB=1
-fi
-make -j ${NO_JOB} 
-
-sudo rm -r /usr/include/opencv4/opencv2
+make -j$(nproc)
 sudo make install
 sudo ldconfig
-
-# cleaning (frees 320 MB)
-make clean
-sudo apt-get update
+sudo apt-get update 
