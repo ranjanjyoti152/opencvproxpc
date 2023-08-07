9cd7336730853f1e11ca387ab24e9eb181e7620f
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-08-07 12:53:36 +0530

    Add files via upload

---
+++ build-opencv-ubuntu 20.04.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y build-essential cmake git unzip pkg-config make
+sudo apt-get install -y python3.8-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get install -y  libjpeg-dev libpng-dev libtiff-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
+mkdir ~/opencv_build && cd ~/opencv_build
+git clone https://github.com/opencv/opencv
+git clone https://github.com/opencv/opencv_contrib
+cd ~/opencv_build/opencv
+mkdir -p build && cd build
+cmake -D WITH_CUDA=OFF -D BUILD_TIFF=ON -D BUILD_opencv_java=OFF -D WITH_OPENGL=ON -D WITH_OPENCL=ON -D WITH_IPP=ON -D WITH_TBB=ON -D WITH_EIGEN=ON -D WITH_V4L=ON -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_opencv_python2=OFF -D CMAKE_INSTALL_PREFIX=/usr/local -D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") -D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_ENABLE_NONFREE=ON -D OPENCV_GENERATE_PKGCONFIG=ON -D PYTHON3_EXECUTABLE=$(which python3) -D PYTHON_DEFAULT_EXECUTABLE=$(which python3) -D OPENCV_EXTRA_MODULES_PATH=~/opencv_build/opencv_contrib/modules -D BUILD_EXAMPLES=ON ..
+make -j8
+sudo make install
