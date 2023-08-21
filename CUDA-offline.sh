25853d19a36c9748d9491e4d68ddc8b1ee3e2516
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-08-22 00:52:12 +0530

    Update CUDA-offline.sh

--- CUDA-offline.sh
+++ CUDA-offline.sh
@@ -49,7 +49,7 @@ sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include
 sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64 
 sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
 sudo apt -y install nvidia-cuda-toolkit
-
+sudo apt-get install libopencv-highgui-dev -y
 echo "###############################################################################################################################################"
 echo "##############################                                                                              ###################################"
 echo "##############################                            INSTALLATION COMPLETE                             ###################################"
