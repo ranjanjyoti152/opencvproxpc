f6b91c12e31acf998aa12c9c385b4e3faab198cd
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-08-23 00:29:35 +0530

    Update CUDA.sh

--- CUDA.sh
+++ CUDA.sh
@@ -53,6 +53,12 @@ sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include
 sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64 
 sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
 sudo apt -y install nvidia-cuda-toolkit
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-get update
+sudo apt-get -y install cuda
 
 echo "###############################################################################################################################################"
 echo "##############################                                                                              ###################################"
