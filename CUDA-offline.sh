029c7d3c512e3a918293be2a06b340825e0e937c
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2024-03-11 21:41:08 -0700

    Update CUDA-offline.sh

--- CUDA-offline.sh
+++ CUDA-offline.sh
@@ -55,7 +55,7 @@ sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt install python3-pip -y
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 
 
 
