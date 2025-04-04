f21f999d7e2103a4497622b80511dfcd951862b0
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2025-04-05 01:52:01 +0530

    Update Harware_report.sh

--- Harware_report.sh
+++ Harware_report.sh
@@ -1,2 +1,13 @@
 ranjanjyoti152
 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
+
+
+
+
+echo "# Vision" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ranjanjyoti152/Vision.git
+git push -u origin main
